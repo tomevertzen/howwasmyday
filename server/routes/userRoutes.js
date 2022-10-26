83ff78bdf3250c743ca98c6282b5c9c2f23e414e
Tom Evertzen <tom_evertzen@hotmail.com>
2022-10-26 21:24:30 +0200

    Added Auth middleware

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 
 const usersController = require("../controllers/userController");
+const { protect } = require("../middleware/authMiddleware");
 
 // @route api/users
 
